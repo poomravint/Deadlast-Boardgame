e400150a48c8de82c6080bbc0810d582740296cf
poomravint <poomravintwongwaro@gmail.com>
2026-04-03 10:24:38 +0700

    Update icon config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  basePath: '/Deadlast-Boardgame',
-  assetPrefix: '/Deadlast-Boardgame/',
+  
   output: "export",
   reactCompiler: true,
 };
