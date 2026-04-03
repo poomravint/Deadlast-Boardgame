2cef22f123bc75c12c51de5ebe32d5a384457433
poomravint <poomravintwongwaro@gmail.com>
2026-04-03 10:21:15 +0700

    Update nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  basePath: '/Deadlast-Boardgame',
+  assetPrefix: '/Deadlast-Boardgame/',
   output: "export",
   reactCompiler: true,
 };
